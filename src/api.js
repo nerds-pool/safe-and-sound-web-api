0363f6c7c43457c541566a9ba0f54090d0fa7bb3
kaveenhyacinth <alwiskaveen@gmail.com>
2021-04-23 13:26:22 +0530

    changes

--- src/api.js
+++ src/api.js
@@ -1,8 +1,5 @@
 const express = require("express");
 const cors = require("cors");
-const rfs = require("rotating-file-stream");
-const morgan = require("morgan");
-const path = require("path");
 const serverless = require("serverless-http");
 
 require("dotenv").config({ path: "./config/.env" });
@@ -11,14 +8,6 @@ require("./db/connect");
 // Init express app
 const app = express();
 
-// Morgan logger stream write -> access.log
-var accessLogStream = rfs.createStream("access.log", {
-  interval: "1d",
-  path: path.join(__dirname, "log"),
-});
-
-// Middlewares
-app.use(morgan("combined", { stream: accessLogStream }));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
@@ -32,7 +21,7 @@ const locationRoute = require("./routes/location.route");
 app.use(`${baseRoute}/auth`, authRoute);
 app.use(`${baseRoute}/user`, userRoute);
 app.use(`${baseRoute}/visit`, visitRoute);
-app.use(`${baseRoute}/location`,locationRoute);
+app.use(`${baseRoute}/location`, locationRoute);
 
 /** Unauthorized error handler */
 app.use(function (err, req, res, next) {
@@ -51,13 +40,5 @@ app.all("*", (req, res) => {
     msg: "404! page not found",
   });
 });
-
-// Server instance
-// const PORT = process.env.PORT ?? 8000;
-// app.listen(PORT, () =>
-//   console.log(
-//     `⚡ [server] Server is running on ${PORT} in ${process.env.NODE_ENV} mode`
-//   )
-// );
 
 module.exports.handler = serverless(app);
